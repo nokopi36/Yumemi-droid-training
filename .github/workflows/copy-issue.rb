ba646ce69c7f7d23f0b24bc5e7ad054db787567e
k_senda <k_senda@yumemi.co.jp>
2023-06-23 18:42:13 +0900

    fix: access token

--- .github/workflows/copy-issue.rb
+++ .github/workflows/copy-issue.rb
@@ -1,17 +1,11 @@
 require 'octokit'
 
-def fetch_token(key)
-  token = ENV.fetch(key, "")
-  # 空文字だと401 Bad Credentials
-  token.empty? ? nil : token
-end
+token = ENV.fetch("access_token", nil)
 
-src_client = Octokit::Client.new(access_token: fetch_token("src_repo_token"))
-src_client.auto_paginate = true
+client = Octokit::Client.new(access_token: token)
+client.auto_paginate = true
+
 src_repo = ENV.fetch("src_repo")
-
-dst_client = Octokit::Client.new(access_token: fetch_token("dst_repo_token"))
-src_client.auto_paginate = true
 dst_repo = ENV.fetch("dst_repo")
 
 def with_retry
@@ -30,23 +24,23 @@ def with_retry
 end
 
 # label一覧の取得
-labels = src_client.labels(src_repo)
+labels = client.labels(src_repo)
 
 # labelを追加
 labels.each do |label|
   with_retry do
-    dst_client.add_label(dst_repo, label.name, label.color, {description: label.description})
+    client.add_label(dst_repo, label.name, label.color, {description: label.description})
   end
   puts "label added: #{label.name}"
 end
 
 # milestone取得
-milestones = src_client.list_milestones(src_repo)
+milestones = client.list_milestones(src_repo)
 
 # milestone追加 & number記録
 milestone_numbers = milestones.map do |m|
   r = with_retry do
-    dst_client.create_milestone(dst_repo, m.title, {description: m.description})
+    client.create_milestone(dst_repo, m.title, {description: m.description})
   end
   puts "milestone added: ##{r.number} #{m.title}"
   [m.number, r.number]
@@ -54,10 +48,10 @@ end.to_h
 
 # issue一覧の取得
 # このAPIではissueとPRは同一視されるがopenなPRは無い前提で実行
-issues = src_client.list_issues(src_repo, {sort: "created", direction: "asc"})
+issues = client.list_issues(src_repo, {sort: "created", direction: "asc"})
 
 # 既存のissue,PRの数を考慮(closed含む)
-issue_number_offset = dst_client.list_issues(dst_repo, {state: "all"}).length + 1
+issue_number_offset = client.list_issues(dst_repo, {state: "all"}).length + 1
 
 # issue追加
 issues.each do |i|
@@ -69,7 +63,7 @@ issues.each do |i|
   milestone_number = milestone_numbers[i.milestone.number]
   labels = i.labels.map(&:name)
   r = with_retry do
-    dst_client.create_issue(dst_repo, i.title, body, {milestone: milestone_number, labels: labels})
+    client.create_issue(dst_repo, i.title, body, {milestone: milestone_number, labels: labels})
   end
   puts "issue added: ##{r.number} #{i.title}"
 end
