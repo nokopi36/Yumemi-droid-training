8b33738b2b1387ae969b32bf36cd39ebc9c68560
RyotaHiyama <r_hiyama@yumemi.co.jp>
2023-10-04 19:03:53 +0900

    update:xmlではなくComposeでレイアウトを作成

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -1,12 +1,29 @@
 package jp.co.yumemi.droidtraining
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            WeatherApp()
+        }
     }
+}
+
+@Composable
+fun WeatherApp() {
+    Text(text = "Hello World!!")
+}
+
+@Preview
+@Composable
+fun PreviewWeatherApp() {
+    WeatherApp()
 }
