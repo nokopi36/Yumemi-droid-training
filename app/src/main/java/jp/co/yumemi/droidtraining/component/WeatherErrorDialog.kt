52b034c92f48d7c2c8e0be0be8b8ff1590c70c73
RyotaHiyama <r_hiyama@yumemi.co.jp>
2023-11-29 20:12:16 +0900

    update:引数とconfirm、dismissが押されたときの処理追加

--- app/src/main/java/jp/co/yumemi/droidtraining/component/WeatherErrorDialog.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/component/WeatherErrorDialog.kt
@@ -4,20 +4,16 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import jp.co.yumemi.droidtraining.R
 
 @Composable
-fun WeatherErrorDialog() {
-    var showErrorDialog by remember {
-        mutableStateOf(true)
-    }
-
+fun WeatherErrorDialog(
+    showErrorDialog: Boolean,
+    confirmButton: () -> Unit,
+    dismissButton: () -> Unit,
+) {
     if (showErrorDialog) {
         AlertDialog(
             onDismissRequest = { /*TODO*/ },
@@ -30,7 +26,7 @@ fun WeatherErrorDialog() {
             confirmButton = {
                 TextButton(
                     onClick = {
-                        showErrorDialog = false
+                        confirmButton()
                     }
                 ) {
                     Text(text = stringResource(id = R.string.reload))
@@ -39,8 +35,9 @@ fun WeatherErrorDialog() {
             dismissButton = {
                 TextButton(
                     onClick = {
-                        showErrorDialog = false
-                    }) {
+                        dismissButton()
+                    }
+                ) {
                     Text(text = stringResource(id = R.string.close))
                 }
             },
@@ -51,5 +48,5 @@ fun WeatherErrorDialog() {
 @Preview
 @Composable
 private fun PreviewWeatherErrorDialog() {
-    WeatherErrorDialog()
+    WeatherErrorDialog(true, confirmButton = {}, dismissButton = {})
 }
