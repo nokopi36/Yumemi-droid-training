380ceae44a914c886e493c8174060c602fc7dd72
RyotaHiyama <r_hiyama@yumemi.co.jp>
2023-12-06 20:04:06 +0900

    add:WeatherAppのViewModel追加

---
+++ app/src/main/java/jp/co/yumemi/droidtraining/WeatherAppViewModel.kt
@@ -0,0 +1,51 @@
+package jp.co.yumemi.droidtraining
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import jp.co.yumemi.api.UnknownException
+import jp.co.yumemi.api.YumemiWeather
+import jp.co.yumemi.droidtraining.data.WeatherState
+
+class WeatherAppViewModel(context: Context) : ViewModel() {
+    private val yumemiWeather = YumemiWeather(context)
+
+    private val _weatherState: MutableLiveData<WeatherState> = MutableLiveData<WeatherState>(
+        WeatherState(
+            R.drawable.sunny,
+            "10",
+            "20",
+            false
+        )
+    )
+    val weatherState: LiveData<WeatherState> = _weatherState
+
+    private val weatherMap = mapOf(
+        "sunny" to R.drawable.sunny,
+        "cloudy" to R.drawable.cloudy,
+        "rainy" to R.drawable.rainy,
+        "snow" to R.drawable.snow
+    )
+
+    val onReloadButtonClicked: () -> Unit = {
+        _weatherState.value = try {
+            val weather = yumemiWeather.fetchThrowsWeather()
+            _weatherState.value?.copy(
+                weather = weatherMap.getOrDefault(
+                    weather,
+                    R.drawable.dummy
+                ),
+                minTemperature = (-5..10).random().toString(),
+                maxTemperature = (20..30).random().toString(),
+                showErrorDialog = false
+            )
+        } catch (e: UnknownException) {
+            _weatherState.value?.copy(showErrorDialog = true)
+        }
+    }
+
+    val onCloseButtonClicked: () -> Unit = {
+        _weatherState.value = _weatherState.value?.copy(showErrorDialog = false)
+    }
+}
