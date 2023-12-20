6f4d31cc002373d6f187b4de4532b4e6c8851f09
RyotaHiyama <r_hiyama@yumemi.co.jp>
2023-12-20 18:45:55 +0900

    update:不要な非同期処理と依存を削除

--- app/src/main/java/jp/co/yumemi/droidtraining/WeatherAppViewModel.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/WeatherAppViewModel.kt
@@ -1,14 +1,11 @@
 package jp.co.yumemi.droidtraining
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import jp.co.yumemi.api.UnknownException
 import jp.co.yumemi.api.YumemiWeather
 import jp.co.yumemi.droidtraining.data.WeatherState
-import jp.co.yumemi.droidtraining.data.WeatherState.Companion.weatherMap
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class WeatherAppViewModel(private val yumemiWeather: YumemiWeather) : ViewModel() {
 
@@ -23,21 +20,19 @@ class WeatherAppViewModel(private val yumemiWeather: YumemiWeather) : ViewModel(
     val weatherState: StateFlow<WeatherState> = _weatherState
 
     val onReloadButtonClicked: () -> Unit = {
-        viewModelScope.launch {
-            _weatherState.value = try {
-                val weather = yumemiWeather.fetchThrowsWeather()
-                _weatherState.value.copy(
-                    weather = weatherMap.getOrDefault(
-                        weather,
-                        R.drawable.dummy
-                    ),
-                    minTemperature = (-5..10).random().toString(),
-                    maxTemperature = (20..30).random().toString(),
-                    showErrorDialog = false
-                )
-            } catch (e: UnknownException) {
-                _weatherState.value.copy(showErrorDialog = true)
-            }
+        _weatherState.value = try {
+            val weather = yumemiWeather.fetchThrowsWeather()
+            _weatherState.value.copy(
+                weather = WeatherState.weatherMap.getOrDefault(
+                    weather,
+                    R.drawable.dummy
+                ),
+                minTemperature = (-5..10).random().toString(),
+                maxTemperature = (20..30).random().toString(),
+                showErrorDialog = false
+            )
+        } catch (e: UnknownException) {
+            _weatherState.value.copy(showErrorDialog = true)
         }
     }
 
