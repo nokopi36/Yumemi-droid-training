855c464f4dbb666f84508eea787d393e201ec02c
RyotaHiyama <r_hiyama@yumemi.co.jp>
2024-03-01 14:45:57 +0900

    update:activityが破棄されても状態を保持できるように

--- app/src/main/java/jp/co/yumemi/droidtraining/WeatherAppViewModel.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/WeatherAppViewModel.kt
@@ -1,26 +1,43 @@
 package jp.co.yumemi.droidtraining
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.yumemi.api.UnknownException
 import jp.co.yumemi.api.YumemiWeather
 import jp.co.yumemi.droidtraining.data.WeatherState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class WeatherAppViewModel @Inject constructor(private val yumemiWeather: YumemiWeather) : ViewModel() {
+class WeatherAppViewModel @Inject constructor(
+    private val yumemiWeather: YumemiWeather,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    private val weatherStateKey = "weatherState"
 
     private val _weatherState: MutableStateFlow<WeatherState> = MutableStateFlow(
-        WeatherState(
+        savedStateHandle[weatherStateKey] ?: WeatherState(
             R.drawable.sunny,
             "10",
             "20",
             false
         )
     )
+
     val weatherState: StateFlow<WeatherState> = _weatherState
+
+    init {
+        viewModelScope.launch {
+            weatherState.collect { weatherState ->
+                savedStateHandle[weatherStateKey] = weatherState
+            }
+        }
+    }
 
     val onReloadButtonClicked: () -> Unit = {
         _weatherState.value = try {
