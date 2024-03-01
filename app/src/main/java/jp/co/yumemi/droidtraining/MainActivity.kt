4afd436ec8e88c2a64b4be7638c88d391f27520c
RyotaHiyama <r_hiyama@yumemi.co.jp>
2024-03-01 14:55:37 +0900

    update:composeにViewModelをInjectionできていなかったため修正

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -2,19 +2,17 @@ package jp.co.yumemi.droidtraining
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.droidtraining.component.WeatherApp
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val weatherAppViewModel: WeatherAppViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherApp(weatherAppViewModel)
+            WeatherApp()
         }
     }
 }
